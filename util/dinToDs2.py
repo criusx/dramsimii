bb448c419c58dcdf8c44de114f419c2c4714b43b
mutien <mutien@mutien>
2010-09-30 20:03:56 -0400

    modified

--- util/dinToDs2.py
+++ util/dinToDs2.py
@@ -4,6 +4,7 @@ freq = 4
 
 misstrace = sys.argv[1]
 ds2trace = sys.argv[2] 
+dintrace = sys.argv[3]
 
 # open miss trace file from dinero for read
 try:
@@ -11,11 +12,18 @@ try:
 except:
     print misstrace + ' does not exists'
 
-# open miss trace file for write in dramsimii foramt
+# open miss trace file for write in dramsimii format
 try:
-    fout = open(ds2trace, 'w')
+    fout0 = open(ds2trace, 'w')
 except:
     print ds2trace + ' does not exists'
+
+# open miss trace file for write in dinero l4 cache
+try:
+    fout1 = open(dintrace, 'w')
+except:
+    print ds2trace + ' does not exists'
+
 
 flag = 0
 for line in fin:
@@ -31,12 +39,15 @@ for line in fin:
         time = float(line[findspace:l-1]) / freq
 
         if req == '0':
-            print >>fout, "%s %s %f %s" %(addr, 'R', time, '0')
+            print >>fout0, "%s %s %f %s" %(addr, 'R', time, '0')
+            print >>fout1, "%s %s %f" %(req, addr, time) 
         elif req == '1':
-            print >>fout, "%s %s %f %s" %(addr, 'W', time, '0')
+            print >>fout0, "%s %s %f %s" %(addr, 'W', time, '0')
+            print >>fout1, "%s %s %f" %(req, addr, time) 
 
     if line.find("Simulation begins") != -1:
         flag = 1
 
 fin.close()
-fout.close()
+fout0.close()
+fout1.close()
--- util/runme.py
+++ util/runme.py
@@ -3,10 +3,12 @@ import os
 
 benchmark = sys.argv[1]
 
+os.system('mkdir stats/' + benchmark)
+
 # ----------------------------------
 # m5 to dinero trace file translator
 # ----------------------------------
-m5todin_commandline = 'python m5ToDin.py trace/' + benchmark + 'Stream.gz trace/' + benchmark + '.din'
+m5todin_commandline = 'python m5ToDin.py ../traces/m5_format/' + benchmark + 'Stream.gz ../traces/dinero_format/' + benchmark + '.din'
 print 'command line: %s' %m5todin_commandline
 print 'm5 to dinero trace file translation begins...'
 os.system(m5todin_commandline)
@@ -16,7 +18,7 @@ print 'done'
 # ----------------------------------
 # run dineroIV to get l3 cache stats
 # ----------------------------------
-l3stats_commandline = './dineroIV -informat d -l1-usize 16M -l1-ubsize 64 -l1-uassoc 16 -l1-upfdist 3 < trace/' + benchmark +'.din > stats/result/' + benchmark + '.l3.txt'
+l3stats_commandline = './dineroIV -informat d -l1-usize 16M -l1-ubsize 64 -l1-uassoc 16 -l1-upfdist 3 < ../traces/dinero_format/' + benchmark +'.din > stats/' + benchmark + '/' + benchmark + '.l3.txt'
 print 'command line: %s' %l3stats_commandline
 print 'getting l3 miss rate...'
 os.system(l3stats_commandline)
@@ -26,20 +28,120 @@ print 'done'
 # ---------------------------------------------------
 # run dineroIV-w-misstrace to generate L3 miss traces
 # ---------------------------------------------------
-l3miss_commandline = './dineroIV-w-misstrace -informat d -l1-usize 16M -l1-ubsize 64 -l1-uassoc 16 -l1-upfdist 3 < trace/' + benchmark + '.din > trace/' + benchmark + '-miss'
+l3miss_commandline = './dineroIV-w-misstrace -informat d -l1-usize 16M -l1-ubsize 64 -l1-uassoc 16 -l1-upfdist 3 < ../traces/dinero_format/' + benchmark + '.din > ../traces/dinero_format/miss/' + benchmark + '-miss'
 print 'command line: %s' %l3miss_commandline
 print 'generating l3 miss traces...'
 os.system(l3miss_commandline)
 print 'done'
 
 
-# --------------------------------
-# generate miss trace file for ds2
-# --------------------------------
-dintods2_commandline = 'python dinToDs2.py trace/' + benchmark + '-miss trace/' + benchmark + '-miss.trace' 
+# ----------------------------------------------
+# generate miss trace file for ds2 and dinero l4
+# ----------------------------------------------
+dintods2_commandline = 'python dinToDs2.py ../traces/dinero_format/miss/' + benchmark + '-miss ../traces/ds2_format/' + benchmark + '-miss.trace ../traces/dinero_format/' + benchmark + '.l4.din'
 print 'command line: %s' %dintods2_commandline
-print 'generating traces for ds2...'
+print 'generating traces for ds2 and dinero l4...'
 os.system(dintods2_commandline)
+print 'done'
+
+
+# ----------------------------------
+# run dineroIV to get l4 cache stats
+# ----------------------------------
+#print 'getting l4 miss rate...'
+
+cacheSize = []
+cacheSize += ['16M']
+cacheSize += ['32M']
+cacheSize += ['64M']
+cacheSize += ['128M']
+cacheSize += ['256M']
+
+blockSize = []
+blockSize += ['64']
+blockSize += ['128']
+blockSize += ['256']
+blockSize += ['512']
+blockSize += ['1024']
+blockSize += ['2048']
+
+associativity = []
+associativity += ['1']
+associativity += ['2']
+associativity += ['4']
+associativity += ['8']
+associativity += ['16']
+associativity += ['32']
+associativity += ['64']
+
+result = 'stats/' + benchmark + '/' + benchmark + '.l4.txt'
+fout = open(result, 'w')
+
+# sweep cache size
+blocksize = '64'
+assoc = '16'
+for cachesize in cacheSize:
+    l4stats_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark +'.l4.din > stats/' + benchmark + '.' + cachesize + '.' + blocksize + '.' + assoc
+    os.system(l4stats_commandline)
+
+    stat = 'stats/' + benchmark + '.' + cachesize + '.' + blocksize + '.' + assoc
+
+    f = open(stat, 'r')
+
+    for line in f:
+        if line.find('Demand Fetches\t') != -1:
+            demandfetches = line[18:36]            
+        if line.find('Demand miss rate') != -1:
+            missrate = line[25:32]
+
+    print >>fout, '%s %s %s %s %s' %(cachesize, blocksize, assoc, missrate, demandfetches)
+
+    f.close()
+
+# sweep block size
+cachesize = '64M'
+assoc = '16'
+for blocksize in blockSize:
+    l4stats_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark +'.l4.din > stats/' + benchmark + '.' + cachesize + '.' + blocksize + '.' + assoc
+    os.system(l4stats_commandline)
+
+    stat = 'stats/' + benchmark + '.' + cachesize + '.' + blocksize + '.' + assoc
+
+    f = open(stat, 'r')
+
+    for line in f:
+        if line.find('Demand Fetches\t') != -1:
+            demandfetches = line[18:36]
+        if line.find('Demand miss rate') != -1:
+            missrate = line[25:32]
+
+    print >>fout, '%s %s %s %s %s' %(cachesize, blocksize, assoc, missrate, demandfetches)
+
+    f.close()
+
+# sweep associativity
+cachesize = '64M'
+blocksize = '16'
+for assoc in associativity:
+    l4stats_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark +'.l4.din > stats/' + benchmark + '.' + cachesize + '.' + blocksize + '.' + assoc
+    os.system(l4stats_commandline)
+
+    stat = 'stats/' + benchmark + '.' + cachesize + '.' + blocksize + '.' + assoc
+
+    f = open(stat, 'r')
+
+    for line in f:
+        if line.find('Demand Fetches\t') != -1:
+            demandfetches = line[18:36]
+        if line.find('Demand miss rate') != -1:
+            missrate = line[25:32]
+
+    print >>fout, '%s %s %s %s %s' %(cachesize, blocksize, assoc, missrate, demandfetches)
+
+    f.close()
+
+fout.close()
+
 print 'done'
 
 
@@ -60,9 +162,10 @@ addressMappingPolicy += ['closepagebaseline']
 addressMappingPolicy += ['closepagebaselineopt']
 addressMappingPolicy += ['closepagelowlocality']
 addressMappingPolicy += ['closepagehighlocality']
+addressMappingPolicy += ['mutien']
 
 for addressmapping in addressMappingPolicy:
-    dinmiss_commandline = 'python dinMiss.py trace/' + benchmark + '-miss trace/' + benchmark + '-miss.din ' + addressmapping
+    dinmiss_commandline = 'python dinMiss.py ../traces/dinero_format/miss/' + benchmark + '-miss ../traces/dinero_format/' + benchmark + '.dimm.din ' + addressmapping
     print 'command line: %s' %dinmiss_commandline
     print 'generating miss traces for dimm caches...'
     os.system(dinmiss_commandline)
@@ -98,17 +201,17 @@ associativity += ['64']
 
 # sweep address mapping policies
 for addressmapping in addressMappingPolicy:
-    result = 'stats/' + benchmark + '.' + addressmapping + '.result.txt'
+    result = 'stats/' + benchmark + '/' + benchmark + '.dimm.' + addressmapping + '.txt'
     fout = open(result, 'w')
 
     # sweep cache size
     blocksize = '64'
     assoc = '16'
     for cachesize in cacheSize:
-        dimmcache0_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < trace/' + benchmark + '-miss.din.' + addressmapping + '.0  > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.0'
-        dimmcache1_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < trace/' + benchmark + '-miss.din.' + addressmapping + '.1 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.1'
-        dimmcache2_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < trace/' + benchmark + '-miss.din.' + addressmapping + '.2 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.2'
-        dimmcache3_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < trace/' + benchmark + '-miss.din.' + addressmapping + '.3 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.3'
+        dimmcache0_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark + '.dimm.din.' + addressmapping + '.0  > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.0'
+        dimmcache1_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark + '.dimm.din.' + addressmapping + '.1 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.1'
+        dimmcache2_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark + '.dimm.din.' + addressmapping + '.2 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.2'
+        dimmcache3_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark + '.dimm.din.' + addressmapping + '.3 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.3'
 
         os.system(dimmcache0_commandline)
         os.system(dimmcache1_commandline)
@@ -165,10 +268,10 @@ for addressmapping in addressMappingPolicy:
     cachesize = '8M'
     assoc = '16'
     for blocksize in blockSize:
-        dimmcache0_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < trace/' + benchmark + '-miss.din.' + addressmapping + '.0  > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.0'
-        dimmcache1_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < trace/' + benchmark + '-miss.din.' + addressmapping + '.1 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.1'
-        dimmcache2_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < trace/' + benchmark + '-miss.din.' + addressmapping + '.2 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.2'
-        dimmcache3_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < trace/' + benchmark + '-miss.din.' + addressmapping + '.3 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.3'
+        dimmcache0_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark + '.dimm.din.' + addressmapping + '.0  > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.0'
+        dimmcache1_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark + '.dimm.din.' + addressmapping + '.1 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.1'
+        dimmcache2_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark + '.dimm.din.' + addressmapping + '.2 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.2'
+        dimmcache3_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark + '.dimm.din.' + addressmapping + '.3 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.3'
 
         os.system(dimmcache0_commandline)
         os.system(dimmcache1_commandline)
@@ -225,10 +328,10 @@ for addressmapping in addressMappingPolicy:
     cachesize = '8M'
     blocksize = '64'
     for assoc in associativity:
-        dimmcache0_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < trace/' + benchmark + '-miss.din.' + addressmapping + '.0  > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.0'
-        dimmcache1_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < trace/' + benchmark + '-miss.din.' + addressmapping + '.1 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.1'
-        dimmcache2_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < trace/' + benchmark + '-miss.din.' + addressmapping + '.2 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.2'
-        dimmcache3_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < trace/' + benchmark + '-miss.din.' + addressmapping + '.3 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.3'
+        dimmcache0_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark + '.dimm.din.' + addressmapping + '.0  > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.0'
+        dimmcache1_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark + '.dimm.din.' + addressmapping + '.1 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.1'
+        dimmcache2_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark + '.dimm.din.' + addressmapping + '.2 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.2'
+        dimmcache3_commandline = './dineroIV -informat d -l1-usize ' + cachesize + ' -l1-ubsize ' + blocksize + ' -l1-uassoc ' + assoc + ' < ../traces/dinero_format/' + benchmark + '.dimm.din.' + addressmapping + '.3 > stats/' + benchmark + '.' + addressmapping + '.' + cachesize + '.' + blocksize + '.' + assoc + '.3'
 
         os.system(dimmcache0_commandline)
         os.system(dimmcache1_commandline)
@@ -281,10 +384,4 @@ for addressmapping in addressMappingPolicy:
         f2.close()
         f3.close()
 
-fout.close()
-
-
-
-# run dineroIV to get L4 cache stats
-#./dineroIV -informat d -l1-usize 64M -l1-ubsize 256 -l1-uassoc 16 < trace/gups-miss.din > stats/l4-cache.txt
-
+    fout.close()
