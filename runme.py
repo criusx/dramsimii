54d0e01b845ec11190cb879c8a4589b31f0efb2a
Joe Gross <criusx@gmail.com>
2010-10-04 17:27:07 -0400

    automatically detect the switch and start recording

--- runme.py
+++ runme.py
@@ -9,15 +9,16 @@ import gzip
 runtype = sys.argv[1]
 numcore = sys.argv[2]
 benchmark = sys.argv[3]
-starttime = sys.argv[4]
+#starttime = sys.argv[4]
+starttime = 2200000000000
 
 dramsimDirectory = os.path.join(os.path.expanduser("~"), 'dramsimii')
 m5directory = os.path.join(os.path.expanduser("~"), 'm5')
 m5binary = os.path.join(m5directory, 'build/ALPHA_FS/m5.opt')
 m5Script = os.path.join(dramsimDirectory, 'm5/configs/example/fs.py')
 
-m5flags = '--trace-flags=Cache --trace-start=' + starttime
-m5scriptFlags = '--detailed -n' + numcore + ' --caches --l3cache -F 10000000000 -b ' + benchmark
+m5flags = '--trace-flags=Cache --trace-start=%d' % (starttime)
+m5scriptFlags = '--detailed -n %s --caches --l3cache -F 1000000000 -b %s' % (numcore, benchmark)
 
 #sedCommand0 = "sed -n -e 's/ReadReq (ifetch)/2/p' -e 's/ReadExReq/0/p' -e 's/ReadReq/0/p' -e 's/Writeback/1/p'"
 #sedCommand1 = "sed -e 's/ [a-z]*$//' -e 's/:.*://'"
@@ -41,6 +42,7 @@ if runtype == '-t':
 	#print out, err
 
 	pattern = re.compile("([0-9]+): system.l3cache: (ReadReq (ifetch)|ReadExReq|ReadReq|Writeback) ([0-9a-f]+).*")
+	switchPattern = re.compile("Switched CPUS @ cycle = [0-9]+")
 
 	filename = "%s_%s.gz" % (benchmark, numcore)
 
@@ -49,38 +51,40 @@ if runtype == '-t':
 		while (True):
 			filename = "%s_%s_%03d.gz" % (benchmark, numcore, counter)
 			if (os.path.isfile(filename) == False):
-				print "going with " + filename
 				outfile = gzip.open(filename, 'wb')
 				break
 			else:
 				counter = counter + 1
-				print "counter at %d" % (counter)
 
 	else:
-		print "did not find"
 		outfile = gzip.open(filename, 'wb')
 
 	lines = []
+	switched = False
 
 	while True:
-		#newLine = out.readline()
 		newLine = p.stdout.readline()
     		if not newLine: break
 
-		#print newLine
-		m = pattern.match(newLine)
-		if m is not None:
-			value = '2'
-			if m.group(2) == "ReadReq" or m.group(2) == "ReadExReq":
-				value = '0'
-			elif m.group(2) == "Writeback":
-				value = '1'
+		if switched == True:
+			m = pattern.match(newLine)
 
-			lines.append(value + " " + m.group(4) + " " + m.group(1))
+			if m is not None:
+				value = '2'
+				if m.group(2) == "ReadReq" or m.group(2) == "ReadExReq":
+					value = '0'
+				elif m.group(2) == "Writeback":
+					value = '1'
 
-			if (len(lines) > 8192):
-				outfile.writelines(lines)
-				lines = []
+				lines.append(value + " " + m.group(4) + " " + m.group(1) + "\n")
+
+				if (len(lines) > 16384):
+					outfile.writelines(lines)
+					lines = []
+		elif switchPattern.match(newLine) is not None:
+			#print 'switching'
+			switched = True
+
 
 	print "Finished running, waiting to finish"
 	p.wait()
